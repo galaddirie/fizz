d0c9faddf992e78d4c8c9f77bfea018ac1631a86
Galad Dirie <galad.work@gmail.com>
2026-03-10 18:20:52 -0400

    remove migrations

--- priv/repo/migrations/20260212045946_drop_users_tokens_table.exs
+++
@@ -1,7 +0,0 @@
-defmodule Fizz.Repo.Migrations.DropUsersTokensTable do
-  use Ecto.Migration
-
-  def change do
-    drop table(:users_tokens)
-  end
-end
--- priv/repo/migrations/20260216204034_remove_sprites_usage_tracking_and_quotas.exs
+++
@@ -1,9 +0,0 @@
-defmodule Fizz.Repo.Migrations.RemoveSpritesUsageTrackingAndQuotas do
-  use Ecto.Migration
-
-  def change do
-    drop table(:workspace_rate_limit_windows)
-    drop table(:workspace_usage_daily)
-    drop table(:workspace_sprite_limits)
-  end
-end
